b5240f10ea6fda56d67c32d05018b9a7ee62b95a
Julio Diaz & Adam Won <student@epicodus.com>
2015-08-10 16:19:59 -0700

    Fixed Translator

--- src/SnoopTranslator.php
+++ src/SnoopTranslator.php
@@ -8,8 +8,10 @@
 			$output = array();
 			foreach($input as $word) {
 				$pos = -1;
-				while(($pos = strpos($word, "s", $pos + 1)) != false) {
-					$word[$pos] = "z";
+				while(($pos = strpos($word, "s", $pos + 1)) !== false) {
+					if(($pos != 0) && (!ctype_upper($word[$pos]))) {
+						$word[$pos] = "z";
+					}
 				}
 				array_push($output, $word);
 			}
